78a2341f653c5750398c2d04879be89495de23cd
Mihail Bodrov <connormiha1@gmail.com>
2016-04-15 01:05:43 +0300

    add undefined check in swap

--- src/swap.ts
+++ src/swap.ts
@@ -5,6 +5,13 @@
  * @param {Number} int2
  */
 function swap(array: Array<number>, int1: number, int2: number): void {
+    if (array[int1] === undefined || array[int2] === undefined) {
+        throw new Error(`
+            int1 is ${int1} - ${array[int1]}
+            int2 is ${int2} - ${array[int2]}
+        `);
+    }
+
     let oldInt1: number = array[int1];
 
     array[int1] = array[int2];
